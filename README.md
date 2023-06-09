b2be2a379661d5901d8f86c413bd15dc7da68b07
Fachrin Aulia <fachrinfan@gmail.com>
2023-06-09 05:32:28 +0200

    $reate README.md
    $Update README.md
    
    $Update README.md
    
    $fix invalid loop execution
    
    add brainfuck easy interpreter

---
+++ README.md
@@ -0,0 +1,16 @@
+# no-brainfuck
+It's just a simple Brainfuck interpreter. So you don't have to brainfuck your brain to interpret a Brainfuck program.
+
+# How to run
+Python3 needs to be installed on your machine, and execute this command:
+```bash
+python3 main.py 'your brainfuck program'
+```
+**Hello World Example**
+```bash
+python3 main.py '>++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<+
++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-
+]<+.'
+```
+
+That's it. [Anyway, what's Brainfuck?](https://esolangs.org/wiki/Brainfuck)
--- interpreter.py
+++ interpreter.py
@@ -3,23 +3,33 @@ def brainfuck_reader(s):
     out, stack = [], []
     blocks = [0 for _ in range(0, 30000)]
 
+    def jump(idx):
+        bracket = 1
+
+        while idx < len(s) and bracket != 0:
+            idx += 1
+            bracket += int(s[idx] == '[')
+            bracket -= int(s[idx] == ']')
+
+        return idx
+
     while i < len(s) and 0 <= ptr < len(blocks):
-        c = s[i]
-
-        ptr += int(c == '>')
-        ptr -= int(c == '<')
-        blocks[ptr] += int(c == '+')
-        blocks[ptr] -= int(c == '-')
-
-        if c == '.':
+        if s[i] == '.':
             out.append(blocks[ptr])
-        elif c == '[' and blocks[ptr] != 0:
+        elif s[i] == '[' and blocks[ptr] != 0:
             stack.append(i)
-        elif c == ']':
-            if blocks[ptr] == 0:
-                stack.pop()
-            else:
-                i = stack[-1]
+        elif s[i] == '[' and blocks[ptr] == 0:
+            i = jump(i)
+        elif s[i] == ']' and blocks[ptr] != 0:
+            i = stack[-1] - 1
+        elif s[i] == ']' and blocks[ptr] == 0:
+            stack.pop()
+        else:
+            ptr += int(s[i] == '>')
+            ptr -= int(s[i] == '<')
+            blocks[ptr] += int(s[i] == '+')
+            blocks[ptr] -= int(s[i] == '-')
+            blocks[ptr] = blocks[ptr] % 256
 
         i += 1
 
